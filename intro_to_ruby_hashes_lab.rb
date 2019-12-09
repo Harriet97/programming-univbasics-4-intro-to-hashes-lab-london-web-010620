e5b6f65f5b51001f1438a90000832e0499b995f9
Harriet Bicknell <harrietindubai@gmail.com>
2019-12-09 12:19:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,8 +23,7 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-    update_hash = 
-    if hash[key]
+    if hash[key] == 0
       hash[key] += 1 
     else hash[key] = 1
   end
